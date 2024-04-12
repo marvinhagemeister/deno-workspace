fbeb6e9a451308d081e51196ddfcbb75f366012f
Marvin Hagemeister <marvin@deno.com>
2024-04-12 15:58:54 +0200

    Repro

---
+++ README.md
@@ -0,0 +1,6 @@
+# Deno workspace bug
+
+Steps to reproduce:
+
+1. Clone this repository
+2. Run `deno run main.ts`
---
+++ main.ts
@@ -0,0 +1,4 @@
+import * as foo from "@marvinh-test/foo";
+import * as bar from "@marvinh-test/bar";
+
+console.log(foo, bar);
